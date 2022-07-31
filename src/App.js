0cdfcd1ed06c901e2be682d7270e453a5db5f6d6
Yash Soni <yashsonijaipur@gmail.com>
2022-07-31 15:07:20 +0530

    build failed issue fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import AboutUs from "./components/AboutUs/AboutUs.jsx";
-import Header from "./components/Header/Header";
+// import Home from "./components/Home/Home";
+// import AboutUs from "./components/AboutUs/AboutUs.jsx";
+// import Header from "./components/Header/Header";
 // import Projects from "./components/Projects/Projects.jsx";
 // import ContactUs from "./components/ContactUs/ContactUs";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import Login from "./components/Login/Login";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-// import { getUsers } from "./action-creators/userAction";
+import { getUsers } from "./action-creators/userAction";
 import { useHistory } from "react-router-dom";
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
   const history = useHistory();
 
   // useEffect(() => {
-  // dispatch(getUsers());
+  //   dispatch(getUsers());
   // }, []);
 
   useEffect(() => {
@@ -27,10 +27,10 @@ function App() {
   return (
     <div>
       <Router>
-        <Header />
+        {/* <Header /> */}
 
         <Switch>
-          <Route exact path="/">
+          {/* <Route exact path="/">
             <Home />
           </Route>
           <Route exact path="/about">
@@ -38,16 +38,16 @@ function App() {
             <AboutUs />{" "}
           </Route>
           <Route exact path="/projects">
-            {/* <Projects /> */}
+            <Projects />
           </Route>
           <Route exact path="/contact">
-            {/* <ContactUs /> */}
-          </Route>
+            <ContactUs />
+          </Route> */}
           <Route exact path="/account">
             <Login />{" "}
           </Route>
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     </div>
   );
